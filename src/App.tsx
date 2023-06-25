b4b7bc8a380c0835df936a2589327d571bc32ae2
ah-munna <ahmunna7x@gmail.com>
2023-06-25 12:21:44 +0600

    fixed errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,88 @@
 import { useState, useCallback, useMemo } from "react";
 import { Box, Grid, Autocomplete, TextField } from "@mui/material";
 import usePlacesAutocomplete, {
-    getGeocode,
-    getLatLng,
+  getGeocode,
+  getLatLng,
 } from "use-places-autocomplete";
 import { useLoadScript } from "@react-google-maps/api";
 
 type Coords = {
-    lat: number;
-    lng: number;
+  lat: number;
+  lng: number;
 };
+
 const libraries: (
-    | "drawing"
-    | "geometry"
-    | "localContext"
-    | "places"
-    | "visualization"
+  | "drawing"
+  | "geometry"
+  | "localContext"
+  | "places"
+  | "visualization"
 )[] = ["places"];
 const PlacesAutoComplete = () => {
-    const [selected, setSelected] = useState<string>("");
-    const [coords, setCoords] = useState<Coords | undefined>();
-    const {
-        ready,
-        setValue,
-        suggestions: { data },
-        clearSuggestions,
-    } = usePlacesAutocomplete();
+  const [coords, setCoords] = useState<Coords | undefined>();
+  const {
+    ready,
+    setValue,
+    suggestions: { data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
 
-    const handleSelect = useCallback(
-        async (address: string) => {
-            setValue(address, false);
-            clearSuggestions();
-            const results = await getGeocode({ address });
-            if (results.length) {
-                setCoords(getLatLng(results[0]));
-            }
-        },
-        [clearSuggestions, getGeocode, getLatLng, setValue]
-    );
+  const handleSelect = useCallback(
+    async (address: string) => {
+      setValue(address, false);
+      clearSuggestions();
+      if (address.length < 2) return;
+      try {
+        const results = await getGeocode({ address });
+        if (results.length) {
+          setCoords(getLatLng(results[0]));
+        }
+      } catch (e) {
+        return;
+      }
+    },
+    [clearSuggestions, getGeocode, getLatLng, setValue]
+  );
 
-    const options = useMemo(
-        () => data.map(({ description }) => description),
-        [data]
-    );
+  const options = useMemo(
+    () => data.map(({ description }) => description),
+    [data]
+  );
 
-    return (
-        <Grid>
-            <p>Lat: {coords?.lat}</p>
-            <p>Lng: {coords?.lng}</p>
-            <Grid>
-                <Autocomplete
-                    disablePortal
-                    id="combo-box-demo"
-                    options={options}
-                    onSelect={async () => await handleSelect(selected)}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            label="Location Name"
-                            value={selected}
-                            onChange={(e) => {
-                                setValue(e.target.value);
-                                setSelected(e.target.value);
-                            }}
-                        />
-                    )}
-                    disabled={!ready}
-                />
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid>
+      <p>Lat: {coords?.lat}</p>
+      <p>Lng: {coords?.lng}</p>
+      <Grid>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={options}
+          onSelect={(e: any) => {
+            setValue(e.target.value);
+            handleSelect(e.target.value);
+          }}
+          renderInput={(params) => (
+            <TextField {...params} label="Location Name" />
+          )}
+          disabled={!ready}
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 function App() {
-    const apiKey = import.meta.env.VITE_API_KEY;
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: apiKey,
-        libraries,
-    });
-    return (
-        <div>
-            <Box>{isLoaded && <PlacesAutoComplete />}</Box>
-        </div>
-    );
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: apiKey,
+    libraries,
+  });
+  return (
+    <div>
+      <Box>{isLoaded && <PlacesAutoComplete />}</Box>
+    </div>
+  );
 }
 
 export default App;
